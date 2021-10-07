2c0d10724fcbff389242beb19fa21b3f8da3950b
RAGOLUpavani <ragolupavani0330@gmail.com>
2021-10-07 11:16:02 +0530

    adding bubble sort program

---
+++ src/com/blz/algorithm/BubbleSort.java
@@ -0,0 +1,36 @@
+package com.blz.algorithm;
+
+public class BubbleSort {
+
+	public static int[] intBubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		
+		return arr;
+
+	} 
+    public static void main(String[] args) {  
+                int arr[] ={3,60,35,2,45,320,5};  
+                 
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                  
+                intBubbleSort(arr);//sorting array elements using bubble sort  
+                 
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+   
+        }  
+}
